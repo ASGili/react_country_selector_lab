edb60d228f800716aad4ecfa5a589d024cf7e49e
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-22 09:45:10 +0100

    map added sort of

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,12 +1,21 @@
 import '../containers/CountriesContainer.css';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 const CountryDetail = ({country}) => {
+    
 
     return (
       <div className="country-detail">
         The capital of {country.name.common} is {country.capital}. 
         <br/>
         Population: {country.population}.
+        <br/>
+        <MapContainer center={[51.505, -0.09]} zoom={10} scrollWheelZoom={false}>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        </MapContainer>
       </div>
     )
 }
